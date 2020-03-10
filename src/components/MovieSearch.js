f3aea2edbef5e022ca61f8cdee02783bd6361c6e
abhi747 <abhi.kk747@gmail.com>
2020-03-11 01:52:58 +0530

    Code refactored

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import AppModal from "./AppModal";
-import { fetchMovieDetails } from "./api";
+import { fetchMovieDetails } from "../config/api";
 
 export class MovieSearch extends Component {
 	state = {
