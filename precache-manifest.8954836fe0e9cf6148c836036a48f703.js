0968683a98103f2f9308661041c1a9b33764f45b
Abhinandan <abhi.kk747@gmail.com>
2020-03-13 02:13:11 +0530

    Updates

---
+++ precache-manifest.8954836fe0e9cf6148c836036a48f703.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab913714dbb5dc817a61292ddc00330",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "195dae1391d43d611422",
+    "url": "/react-movie-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "576cc2dcebc07674ec81",
+    "url": "/react-movie-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "195dae1391d43d611422",
+    "url": "/react-movie-app/static/js/2.499d88e8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/react-movie-app/static/js/2.499d88e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576cc2dcebc07674ec81",
+    "url": "/react-movie-app/static/js/main.c24adb08.chunk.js"
+  },
+  {
+    "revision": "9167af0df8dbe5f97404",
+    "url": "/react-movie-app/static/js/runtime-main.8f15e600.js"
+  }
+]);
