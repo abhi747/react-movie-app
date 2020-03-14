e6ce15f6dad9b90f1cf17ae421a98893cefabc52
Abhinandan <abhi.kk747@gmail.com>
2020-03-14 12:57:18 +0530

    Updates

--- precache-manifest.f5b18f9abc755644b4b9a59b990c56ae.js
+++ precache-manifest.f5b18f9abc755644b4b9a59b990c56ae.js
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-movie-app/static/js/2.a97ce77f.chunk.js"
   },
   {
-    "revision": "ddcf63d2240c2269b5dd773c723c866b",
+    "revision": "7755b49a72efba372223191421272cfc",
     "url": "/react-movie-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-movie-app/precache-manifest.f4ef752149b65f5c39b80977b31513db.js"
+  "/react-movie-app/precache-manifest.f5b18f9abc755644b4b9a59b990c56ae.js"
 );
 
 workbox.clientsClaim();
