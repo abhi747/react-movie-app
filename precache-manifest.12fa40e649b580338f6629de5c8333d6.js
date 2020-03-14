0339beb1bde5c9ff77ab46b0a384e6a07c4f299f
Abhinandan <abhi.kk747@gmail.com>
2020-03-14 12:24:46 +0530

    Updates

---
+++ precache-manifest.12fa40e649b580338f6629de5c8333d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "358f54b5d71685183672",
+    "url": "/react-movie-app/static/css/main.ce38b284.chunk.css"
+  },
+  {
+    "revision": "358f54b5d71685183672",
+    "url": "/react-movie-app/static/js/main.fff64142.chunk.js"
+  },
+  {
+    "revision": "e0c3c7f83a8b3d0de128",
+    "url": "/react-movie-app/static/js/runtime~main.4dfe89d3.js"
+  },
+  {
+    "revision": "3868e805ad5b910eace5",
+    "url": "/react-movie-app/static/css/2.1a69ec0b.chunk.css"
+  },
+  {
+    "revision": "3868e805ad5b910eace5",
+    "url": "/react-movie-app/static/js/2.a97ce77f.chunk.js"
+  },
+  {
+    "revision": "b5bc764b82cfb86adb636b32c53f9ab1",
+    "url": "/react-movie-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-movie-app/precache-manifest.8954836fe0e9cf6148c836036a48f703.js"
+  "/react-movie-app/precache-manifest.12fa40e649b580338f6629de5c8333d6.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-movie-app/index.html"), {
+workbox.routing.registerNavigationRoute("/react-movie-app/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
